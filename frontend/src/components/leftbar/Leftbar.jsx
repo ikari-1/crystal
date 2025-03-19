19573716c147d68c053046fae8d2e3198320def8
ynaoton410 <y.naoton410@gmail.com>
2025-03-19 18:58:45 +0900

    コンポーネントを細分化しました

---
+++ frontend/src/components/leftbar/Leftbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Leftbar.css"
+
+export default function Leftbar() {
+  return (
+    <>
+      <div className="leftbar">
+        <button className="newPostBtn">新規投稿</button>
+      </div>
+    </>
+  )
+}
