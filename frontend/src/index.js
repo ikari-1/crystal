207cc1f21e57c59960f488da73ed7b7289744c07
ikari <mn.y.ikari@gmail.com>
2025-10-19 18:25:21 +0900

    axios設定を間違えてた。baseURLを設定するように修正。

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import ThemeProvider from './context/ThemeContext';
 // 本番Vercel環境ではproxy機能がないため、相対パスが自動変換されず、バックエンドと通信できない。この問題を解決するためにaxiosのbaseURLを設定する。
 import axios from 'axios';
 if (process.env.NODE_ENV === 'production') {
-  axios.defaults.url = process.env.REACT_APP_API_URL;
+  axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
