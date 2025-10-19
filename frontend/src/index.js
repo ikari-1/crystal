53b6f3ad847801bc27bebe24ec1eaab67ac10b92
ikari <mn.y.ikari@gmail.com>
2025-10-19 18:02:11 +0900

    axiosのbaseURLを設定し、Vercel環境でのバックエンド通信の問題を解決

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,11 @@ import App from './App';
 import { AuthContextProvider } from './context/AuthContext';
 import { SearchProvider } from "./context/SearchContext";
 import ThemeProvider from './context/ThemeContext';
+// 本番Vercel環境ではproxy機能がないため、相対パスが自動変換されず、バックエンドと通信できない。この問題を解決するためにaxiosのbaseURLを設定する。
+import axios from 'axios';
+if (process.env.NODE_ENV === 'production') {
+  axios.defaults.url = process.env.REACT_APP_API_URL;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
